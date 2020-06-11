5a4696eecefd6a4df46ebaeae611c9858cfc5088
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-06-11 16:56:46 -0300

    codigo_em_manutencao

--- CustomStreamListener.py
+++ CustomStreamListener.py
@@ -11,77 +11,56 @@ class CustomStreamListener(tweepy.StreamListener):
 
         if not hasattr(status, "retweeted_status"):
 
-            try:                
-
+            try:
+                
                 print(status.extended_tweet["full_text"])
 
-                print("id_tweet > 140ch:" + status.id_str)
+                print("id_tweet > 140ch = " + status.id_str)
 
-                print("Tamanho = " + str(len(status.extended_tweet["full_text"]))) 
+                print("Localização Usuário = " + str(status.user.location)) 
+
+                print("Tamanho = " + str(len(status.extended_tweet["full_text"])))
 
                 print("\n")
 
-                if(not tweet_repetido_extracao08_tweet_maior_que_140_caracteres(status.id_str)):
+                if( not tweet_repetido_extracao08_tweet_maior_que_140_caracteres(status.id_str)):
 
-                    tweet_maior_140ch = True
+                    tweet_maior_140ch = True                
 
-                    insere_mongodb(status,tweet_maior_140ch)  
+                    insere_mongodb(status,tweet_maior_140ch)
+                else:
+                    print('tweet > 140 repetido | id = ' + status.id_str)
 
-            except AttributeError:                
+            except AttributeError:
 
-                print(status.text)  
+                print(status.text) 
 
-                print("id_tweet <= 140ch:" + status.id_str)
+                print("id_tweet <= 140ch = " + status.id_str)
 
-                print("Tamanho = " + str(len(status.text))) 
+                print("Localização Usuário = " + str(status.user.location)) 
 
-                print("\n")             
+                print("Tamanho = " + str(len(status.text)))
 
-                if(not tweet_repetido_extracao08_tweet_menor_igual_140_caracteres(status.id_str)):
+                print("\n")
 
-                    tweet_menor_140ch = False    
+                if( not tweet_repetido_extracao08_tweet_menor_igual_140_caracteres(status.id_str)):
 
-                    insere_mongodb(status,tweet_menor_140ch)
+                    tweet_maior_140ch = False
 
+                    insere_mongodb(status,tweet_maior_140ch)
+                else:
+                    print('tweet <= 140 repetido | id = ' + status.id_str)
 
     def on_error(self, status_code):
         print("Erro com o código:", status_code)
         return True
 
 
-def tweet_repetido_extracao08_tweet_maior_que_140_caracteres(id_tweet):
-    for tweet in db.extracao08_tweet_maior_que_140_caracteres.find():
-        if(tweet['id_tweet'] == str(id_tweet)):
-            return True
-    return False
-
-
-def tweet_repetido_extracao08_tweet_menor_igual_140_caracteres(id_tweet):
-    for tweet in db.extracao08_tweet_menor_igual_140_caracteres.find():
-        if(tweet['id_tweet'] == str(id_tweet)):
-            print("repetido\n")
-            return True
-    return False
-
-
-def get_tweets_usuario(nome_usuario):
-
-    tweets = []
-    api = tweepy.API(auth)
-    retweeted = "RT"
-    marcacao = "@"
-
-    for status in tweepy.Cursor(api.user_timeline, id=str(nome_usuario)).items(100):
-        if(retweeted and marcacao not in status.text):
-            tweets.append(status.text)
-
-    return tweets
-
 
 def insere_mongodb(status, tweet_maior_140ch):
     if(tweet_maior_140ch):
         db.extracao08_tweet_maior_140_caracteres.insert_one({
-            "parametros_busca": "ansiedade,depressão,saúde mental,depressão,saude mental",
+            "parametros_busca": "ansiedade,depressão,saúde mental,depressao,saude-mental",
             "data": "10-06-2020-08-00-am",
             "tipo": "tweets maiores que 140 caracteres",
             "id_usuario": status.user.id_str,
@@ -99,7 +78,7 @@ def insere_mongodb(status, tweet_maior_140ch):
         })
     else:
         db.extracao08_tweet_menor_igual_140_caracteres.insert_one({
-            "parametros_busca": "ansiedade,depressão,saúde mental,depressão,saude mental",
+            "parametros_busca": "ansiedade,depressão,saúde mental,depressao,saude-mental",
             "data": "10-06-2020-08-00-am",
             "tipo": "tweets menores que 140 caracteres",
             "id_usuario": status.user.id_str,
@@ -115,3 +94,32 @@ def insere_mongodb(status, tweet_maior_140ch):
             "data_criacao_conta_usuario": status.user.created_at,
             "tweets_usuario":  get_tweets_usuario(status.user.screen_name)
         })
+
+
+def tweet_repetido_extracao08_tweet_maior_que_140_caracteres(id_tweet):
+    for tweet in db.extracao08_tweet_maior_140_caracteres.find():
+        if(tweet['id_tweet'] == str(id_tweet)):            
+            return True    
+    return False
+
+
+def tweet_repetido_extracao08_tweet_menor_igual_140_caracteres(id_tweet):
+    for tweet in db.extracao08_tweet_menor_igual_140_caracteres.find():
+        if(tweet['id_tweet'] == str(id_tweet)):            
+            return True   
+    return False
+
+
+def get_tweets_usuario(nome_usuario):
+
+    tweets = []
+    api = tweepy.API(auth)
+    retweeted = "RT"
+    marcacao = "@"
+
+    for status in tweepy.Cursor(api.user_timeline, id=str(nome_usuario)).items(100):
+        if(retweeted and marcacao not in status.text):
+            tweets.append(status.text)
+
+    return tweets
+
--- Tweet.py
+++ Tweet.py
@@ -6,36 +6,37 @@ from AutenticacaoMongoDb import AutenticacaoMongoDb
 db = AutenticacaoMongoDb.autenticaERetornaInstanciaDoBancoDeDados()
 auth = AutenticacaoApiTwitter.autentica()
 
+
 class Tweet():
 
     @staticmethod
-    def extrair_tweets(quantidade_parametros):
+    def extrair_tweets():
 
-        parametros = []
-        contador = 1
+        parametros = ['ansiedade', 'depressão','saúde mental', 'depressao', 'saude mental']
+        #contador = 1
 
-        while contador <= quantidade_parametros:
-            parametro = input("Informe o " + str(contador) + "º parâmetro: ")
-            parametros.append(parametro)
-            contador += 1
+        # while contador <= quantidade_parametros:
+        # parametro = input("Informe o " + str(contador) + "º parâmetro: ")
+        # parametros.append(parametro)
+        # contador += 1
 
         print("\nExtraindo tweets...")
 
         streaming_api = tweepy.streaming.Stream(auth, CustomStreamListener())
         streaming_api.filter(follow=None, track=parametros, languages=["pt"])
-        
+
         print("\nExtração concluída com sucesso!")
 
     @staticmethod
     def imprimir_tweets():
 
-        for tweet in db.extracao08_tweet_maior_que_140_caracteres.find():
+        for tweet in db.extracao08_tweet_maior_140_caracteres.find():
             print('\n')
             print('Tweet: ' + str(tweet['tweet']))
 
     @staticmethod
     def count_tweets():
-        return db.extracao08_tweet_maior_que_140_caracteres.count_documents({})
+        return db.extracao08_tweet_maior_140_caracteres.count_documents({})
 
     @staticmethod
     def gerar_txt_tweets():
@@ -44,14 +45,15 @@ class Tweet():
 
         count = 1
 
-        arquivo = open('test.txt', 'w', encoding='utf-8')
+        arquivo = open('extracao08_tweet_maior_140_caracteres.txt','w', encoding='utf-8')
 
-        for tweet in db.extracao08_tweet_maior_que_140_caracteres.find():
+        for tweet in db.extracao08_tweet_maior_140_caracteres.find():
             arquivo.write('\n')
             arquivo.write('-----------------------------------------------------------------------------------------------------------------------------------------')
             arquivo.write('\n')
             arquivo.write('\n')
-            arquivo.write('('+str(count) + ')' + ' - ' + tweet['id_tweet'])
+            arquivo.write('id = ' + tweet['id_tweet'])
+            arquivo.write('\n('+str(count) + ')' + ' - ' + tweet['tweet'])
             arquivo.write('\n')
             count += 1
 
@@ -59,4 +61,87 @@ class Tweet():
 
         print("Arquivo Gerado com Sucesso!")
 
-    
+    def tweet_repetido_extracao08_tweet_maior_que_140_caracteres(id_tweet):
+        for tweet in db.extracao08_tweet_maior_140_caracteres.find():
+            if(tweet['id_tweet'] == str(id_tweet)):
+                print("repetido")
+                return True
+        print(" nao repetido")
+        return False
+
+    @staticmethod
+    def copiar_colecao():
+
+        count = 0
+
+        for tweet in db.extracao07_tweet_maior_que_140_caracteres.find():
+
+            if(not tweet_repetido_extracao08_tweet_maior_que_140_caracteres(tweet["id_tweet"])):
+
+                print(tweet["id_tweet"] + ' inserido')
+
+                db.extracao08_tweet_maior_140_caracteres.insert_one({
+                    "parametros_busca": tweet["parametros_busca"],
+                    "data": tweet["data"],
+                    "tipo": tweet["tipo"],
+                    "id_usuario": tweet["id_usuario"],
+                    "nome_usuario": tweet["nome_usuario"],
+                    "id_tweet": tweet["id_tweet"],
+                    "tweet": tweet["tweet"],
+                    "quantidade_caracteres": tweet["quantidade_caracteres"],
+                    "data_criacao_tweet": tweet["data_criacao_tweet"],
+                    "quantidade_retweets": tweet["quantidade_retweets"],
+                    "localizacao_usuario": tweet["localizacao_usuario"],
+                    "descricao_usuario": tweet["descricao_usuario"],
+                    "quantidade_seguidores_usuario": tweet["quantidade_seguidores_usuario"],
+                    "data_criacao_conta_usuario": tweet["data_criacao_conta_usuario"],
+                    "tweets_usuario": tweet["tweets_usuario"]
+                })
+
+                count += 1
+
+                print(str(count))
+
+    @staticmethod
+    def get_tweets_usuario(id_usuario):
+
+        tweets = []
+        api = tweepy.API(auth)
+        retweeted = "RT"
+        marcacao = "@"
+        count = 1
+
+        usuario =  api.get_user(id_usuario)
+
+        arquivo = open('dados_usuario_pedrooneto.txt', 'w', encoding='utf-8')
+        
+        arquivo.write('Id do Usuário : ' + str(usuario.id_str))
+        arquivo.write('\nNome Usuário : ' + str(usuario.name))
+        arquivo.write('\nScreen Name : ' + str(usuario.screen_name))
+        arquivo.write('\nLocalização : ' + str(usuario.location))
+        arquivo.write('\nDescrição : ' + str(usuario.description))
+        arquivo.write('\nQuantidade de Seguidores : ' + str(usuario.followers_count))
+        arquivo.write('\nQuantidade de Pessoas Seguidas : ' + str(usuario.friends_count))
+        arquivo.write('\nQuantidade de Tweets Postados : ' + str(usuario.statuses_count))
+        arquivo.write('\nData da Criação da Conta : ' + str(usuario.created_at))
+        
+        arquivo.write('\n\nLISTA DAS ÚLTIMAS POSTAGENS\n\n')
+                
+        for status in tweepy.Cursor(api.user_timeline, id=str(id_usuario)).items(500):            
+            if(retweeted and marcacao not in status.text):
+                arquivo.write('-----------------------------------------------------------------------------------------------------------------------------------------')
+                arquivo.write('\n')
+                arquivo.write('('+str(count) + ')' + ' - ' + str(status.text)) 
+                arquivo.write('\n')
+                count+=1
+        
+        print("\nArquivo Gerado com Sucesso!\n")
+
+
+
+def tweet_repetido_extracao08_tweet_maior_que_140_caracteres(id_tweet):
+    for tweet in db.extracao08_tweet_maior_140_caracteres.find():
+        if(tweet['id_tweet'] == str(id_tweet)):
+            return True
+    return False
+
